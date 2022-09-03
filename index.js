64dfa805cad2d15e5d7e097e2f85d8611e2fc6ea
rana135 <rana286090@gmail.com>
2022-09-03 23:45:40 +0600

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 // app.use(viewCount)
 // app.use(limiter)
 
-app.use('/user/all', userRoutes)
+app.use('https://assingment-1-server.herokuapp.com/user/all', userRoutes)
 
 
 dbConncet()
